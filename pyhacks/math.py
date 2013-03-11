b2369962162d947535b458cf3a8ec7ccad95b928
Renan Birck Pinheiro <renan.ee.ufsm@gmail.com>
2013-03-11 14:28:59 -0300

    misc. functions.

--- pyhacks/math.py
+++ pyhacks/math.py
@@ -4,6 +4,8 @@
 # python-hacks: A bunch of small hacks and snippets that I found useful.
 
 def getSpectrum(y,Fs):
+    from numpy import arange
+    from numpy.fft import fft
     """
     Does the FFT of signal y, sampled at Fs samples per second,
     and returns a list of tuples (frequency,value).
@@ -21,3 +23,18 @@ def getSpectrum(y,Fs):
     Y = Y[range(n/2)]
     return zip(frq, abs(Y))
 
+
+def unit_step(x):
+    from numpy import asarray
+    return asarray(x>=0,dtype=float)
+
+def agm(x,y,eps=1e-11):
+    from math import sqrt
+
+    if a*g < 0:
+	    raise ValueError
+    [a, g] = [(x+y)/2.0,sqrt(x*y)]
+    while(abs(a-g) > eps):
+        [a,g] = [(a+g)/2.0,sqrt(a*g)]
+		        
+    return a
